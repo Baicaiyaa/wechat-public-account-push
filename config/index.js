0ed7c8312ec40cafac7dcd75f72372b3f53f55c0
Baicaiyaa <112292734+Baicaiyaa@users.noreply.github.com>
2022-08-28 09:52:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     // 所在省份
     province: "江西",
     // 所在城市
-    city: "上饶广丰",
+    city: "上饶",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "豆沙包", "year": "2002", "date": "06-13", "type": 'new'},
