2cf8ff831858d13abb003468ccd045647f037494
Baicaiyaa <112292734+Baicaiyaa@users.noreply.github.com>
2022-08-28 09:48:48 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "Z_aS-MLJxUTCtW4f6wf01uDbRaghoBAJiwRfbFZCxT4",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [wxid_rfpyurskijdo22],
+    user: ["wxid_rfpyurskijdo22","wxid_z1m28o3qqo4h22"],
      
     // 信息配置
     // 所在省份
