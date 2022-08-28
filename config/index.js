1093e77106031c193f5b8dac477a37e89f2064b3
Baicaiyaa <112292734+Baicaiyaa@users.noreply.github.com>
2022-08-28 09:40:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,18 +11,16 @@ export const config = {
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "江西",
     // 所在城市
-    city: "惠州",
+    city: "广丰",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "豆沙包", "year": "2002", "date": "06-13", "type": 'new'},
+      {"name": "大白菜", "year": "1999", "date": "05-20", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
+    loveDate: "2017-08-11",
     }
 
 // {{date.DATA}}  
@@ -31,7 +29,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
